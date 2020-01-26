c4ae38e83f32b1b157ddb03b528d6fbe0a11868f
kann-s <sriramkannan72@gmail.com>
2020-01-26 18:17:03 +0000

    get product routes done

--- app/api/products.py
+++ app/api/products.py
@@ -1,13 +1,19 @@
 from app.api import bp
+from app.models import Product
 
+from flask import jsonify
+from flask import request
 
 @bp.route('/products', methods=['GET'])
 def get_products():
-    return {}
+    page = request.args.get('page', 1, type=int)
+    per_page = min(request.args.get('per_page', 10, type=int), 100)
+    data = Product.to_collection_dict(Product.query, page, per_page)
+    return jsonify(data)
 
 @bp.route('/product/<int:id>', methods=['GET'])
 def get_product(id):
-    return {}
+    return jsonify(Product.query.get_or_404(id).to_dict())
 
 @bp.route('/product', methods=['POST'])
 def create_product():
--- app/models.py
+++ app/models.py
@@ -2,12 +2,32 @@ from app import db
 import json
 
 
-class Product(db.Model):
+class PaginatedAPIMixin(object):
+    @staticmethod
+    def to_collection_dict(query, page, per_page):
+        resources = query.paginate(page, per_page, False)
+        data = [ item.to_dict() for item in resources.items ]
+        return data
+
+class Product(PaginatedAPIMixin, db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     product_code = db.Column(db.String(10), index=True, unique=True)
     product_name = db.Column(db.String(120), index=True)
     price = db.Column(db.Float)
+
+    def to_dict(self):
+        data = {
+            'id': self.id,
+            'name': self.product_name,
+            'price': self.price,
+        }
+        return data
+
+    def from_dict(self, data):
+        for field in ['code', 'name', 'price']:
+            if field in data:
+                setattr(self, field, data[field])
 
     def __repr__(self):
         return json.dumps({
